3fb055b0ecd7f33ccadaf0d54fd2dfe0b5ca6e17
fahim-arif <fahim1.618555@gmail.com>
2023-05-29 21:06:07 +0600

    top bar ui complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,10 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {},
+  extend: {
+    colors: {
+      customPink: "#FF2B85",
+    },
   },
   plugins: [],
 };
