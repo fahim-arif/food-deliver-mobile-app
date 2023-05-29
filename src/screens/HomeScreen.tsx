68d4b1b52d5091cdd0b51cc9f8faad94c2d8c908
fahim-arif <fahim1.618555@gmail.com>
2023-05-29 21:16:15 +0600

    restructuring project

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,16 +1,9 @@
-import { SafeAreaView, Text, View } from "react-native";
-import React, { useLayoutEffect, useState } from "react";
+import { View } from "react-native";
+import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import Icon from "react-native-vector-icons/FontAwesome";
-import { SearchBar } from "@rneui/themed";
+import Header from "../components";
 
 export default function HomeScreen() {
-  const [search, setSearch] = useState("");
-
-  const updateSearch = (query: string) => {
-    setSearch(query);
-    // add your search handling logic here
-  };
   const navigation = useNavigation();
 
   useLayoutEffect(() => {
@@ -20,56 +13,8 @@ export default function HomeScreen() {
   }, [navigation]);
 
   return (
-    <View className="bg-pink-600">
-      <SafeAreaView className="mt-10 bg-pink-600 pb-6">
-        <View className="flex-row w-full justify-between p-4 bg-pink-600 items-center">
-          <View className="text-lg">
-            <Icon size={20} name="bars" color={"#ffffff"} />
-          </View>
-          <View className="flex-1 ml-6">
-            <Text className="text-white text-sm font-bold">
-              Current Location
-            </Text>
-            <Text className="text-white text-xs">
-              Gohail Road, CO-office, Bogura
-            </Text>
-          </View>
-          <View className="flex-row">
-            <Icon size={20} color={"#fff"} name="heart-o" />
-            <Icon
-              style={{ paddingLeft: 10 }}
-              size={20}
-              name="shopping-cart"
-              color={"#fff"}
-            />
-          </View>
-        </View>
-
-        <View className="px-2">
-          <SearchBar
-            placeholder="Search for shop & resturants"
-            onChangeText={updateSearch}
-            value={search}
-            inputContainerStyle={{
-              backgroundColor: "white",
-              borderBottomWidth: 0,
-              borderTopWidth: 0,
-              borderRadius: 30,
-            }}
-            inputStyle={{
-              fontSize: 12,
-            }}
-            containerStyle={{
-              backgroundColor: "transparent",
-              borderBottomWidth: 0,
-              borderTopWidth: 0,
-            }}
-            leftIconContainerStyle={{
-              marginLeft: 15,
-            }}
-          />
-        </View>
-      </SafeAreaView>
+    <View>
+      <Header />
     </View>
   );
 }
